4bffaccff743aac06a391dd6c511846350471684
Sebastian Graf <sgraf1337@gmail.com>
2026-02-17 13:45:20 +0000

    measure grind separately

--- Loom/Tactic/Bench.lean
+++ Loom/Tactic/Bench.lean
@@ -16,23 +16,30 @@ def loop (n : Nat) : StateM Nat Unit := do
 
 def Goal (n : Nat) : Prop := ∀ s post (_ : post s), wp (loop n) (fun _ => post) () s
 
-def driver (n : Nat) (check := true) (k : MVarId → MetaM Unit) : MetaM Unit := do
+def driver (n : Nat) (check := true) (k : MVarId → MetaM (List MVarId)) : MetaM Unit := do
   let goal :=mkApp (mkConst ``Goal) (mkNatLit n)
   let mvar ← mkFreshExprMVar goal
-  let ([mvarId], _) ← Lean.Elab.runTactic mvar.mvarId! (← `(tactic| simp only [Goal, loop, step])).raw {} {} |
-    throwError "FAILED!"
+  let ([mvarId], _) ← Lean.Elab.runTactic mvar.mvarId! (← `(tactic| simp only [Goal, loop, step])).raw {} {}
+    | throwError "FAILED!"
   let startTime ← IO.monoNanosNow
-  k mvarId
+  let goals ← k mvarId
   let endTime ← IO.monoNanosNow
-  let ms := (endTime - startTime).toFloat / 1000000.0
+  let solveMs := (endTime - startTime).toFloat / 1000000.0
+  let startTime ← IO.monoNanosNow
+  -- logInfo m!"goals: {goals}"
+  for goal in goals do
+    let ([], _) ← Lean.Elab.runTactic goal (← `(tactic| grind)).raw {} {}
+      | throwError "Could not discharge goal with `grind`: {goal}"
+  let endTime ← IO.monoNanosNow
+  let grindMs := (endTime - startTime).toFloat / 1000000.0
   if check then
     let startTime ← IO.monoNanosNow
     checkWithKernel (← instantiateExprMVars mvar)
     let endTime ← IO.monoNanosNow
     let kernelMs := (endTime - startTime).toFloat / 1000000.0
-    IO.println s!"goal_{n}: {ms} ms, kernel: {kernelMs} ms"
+    IO.println s!"goal_{n}: {solveMs} ms, grind: {grindMs} ms, kernel: {kernelMs} ms"
   else
-    IO.println s!"goal_{n}: {ms} ms"
+    IO.println s!"goal_{n}: {solveMs} ms, grind: {grindMs} ms"
 
 macro "solve" : tactic => `(tactic| {
   intro s post h
@@ -41,7 +48,7 @@ macro "solve" : tactic => `(tactic| {
 
 def solveUsingMeta (n : Nat) (check := true) : MetaM Unit := do
   driver n check fun mvarId => do
-    let ([], _) ← Lean.Elab.runTactic mvarId (← `(tactic| solve)).raw {} {} | throwError "FAILED!"
+    Prod.fst <$> Lean.Elab.runTactic mvarId (← `(tactic| (intro s post h; mvcgen'))).raw {} {}
 
 def runBenchUsingMeta (sizes : List Nat) : MetaM Unit := do
   IO.println "=== VCGen tests ==="
