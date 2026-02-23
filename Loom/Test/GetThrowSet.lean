17eb3f9ddfc72fa5f4daf4fb604a1ce129815322
volodeyka <vovaglad00@gmail.com>
2026-02-23 13:23:08 +0800

    fix benchmarks

--- Loom/Test/GetThrowSet.lean
+++ Loom/Test/GetThrowSet.lean
@@ -4,66 +4,25 @@ import Loom.Tactic.VCGen
 
 open Loom Lean Meta Order
 
-@[lspec] theorem spec_get_StateT' {m : Type u → Type v} {l e : Type u}
-    [CompleteLattice l] [Monad m] [LawfulMonad m] [WPMonad m l e]
-    {σ : Type u} (post : σ → σ → l) (epost : e) :
-    (fun s => post s s) ⊑ wp (get : StateT σ m σ) post epost :=
-  WP.get_StateT_wp post epost
+abbrev M := ExceptT String <| StateM Nat
 
-@[lspec] theorem spec_set_StateT' {m : Type u → Type v} {l e : Type u}
-    [CompleteLattice l] [Monad m] [LawfulMonad m] [WPMonad m l e]
-    {σ : Type u} (x : σ) (post : PUnit → σ → l) (epost : e) :
-    (fun _ => post ⟨⟩ x) ⊑ wp (set x : StateT σ m PUnit) post epost := by
-  exact WP.set_StateT_wp x post epost
+@[lspec] theorem spec_throw (e : String) {post : α → Nat → Prop} {epost : EPost⟨String → Nat → Prop⟩} :
+    epost.1 e ⊑ wp (throw (m := M) e) post epost := PartialOrder.rel_refl
 
-@[lspec] theorem spec_monadLift_ExceptT {m : Type u → Type v} {l e : Type u}
-    [CompleteLattice l] [Monad m] [LawfulMonad m] [WPMonad m l e]
-    {ε α : Type u} (x : m α) (post : α → l) (epost : (ε → l) × e) :
-    wp x post epost.2 ⊑ wp (MonadLift.monadLift x : ExceptT ε m α) post epost := by
-  exact WP.monadLift_ExceptT_wp x post epost
+@[lspec] theorem spec_set (x : Nat) {post : PUnit → Nat → Prop} {epost : EPost⟨String → Nat → Prop⟩} :
+    (fun _ => post ⟨⟩ x) ⊑ wp (set (m := M) x) post epost := PartialOrder.rel_refl
 
-@[lspec] theorem spec_MonadStateOf_get_ExceptT {m : Type u → Type v} {l e : Type u}
-    [CompleteLattice l] [Monad m] [LawfulMonad m] [WPMonad m l e]
-    [MonadStateOf σ m] (post : σ → l) (epost : (ε → l) × e) :
-    wp (MonadLift.monadLift (MonadStateOf.get : m σ) : ExceptT ε m σ) post epost ⊑
-      wp (MonadStateOf.get : ExceptT ε m σ) post epost := by
-  rfl
-
-@[lspec] theorem spec_MonadStateOf_set_ExceptT {m : Type u → Type v} {l e : Type u}
-    [CompleteLattice l] [Monad m] [LawfulMonad m] [WPMonad m l e]
-    [MonadStateOf σ m] (x : σ) (post : PUnit → l) (epost : (ε → l) × e) :
-    wp (MonadLift.monadLift (MonadStateOf.set (σ := σ) x : m PUnit) : ExceptT ε m PUnit) post epost ⊑
-      wp (MonadStateOf.set (σ := σ) x : ExceptT ε m PUnit) post epost := by
-  rfl
-
-@[lspec] theorem spec_get_ExceptT {m : Type u → Type v} {l e : Type u}
-    [CompleteLattice l] [Monad m] [LawfulMonad m] [WPMonad m l e]
-    [MonadStateOf σ m] (post : σ → l) (epost : (ε → l) × e) :
-    wp (MonadLift.monadLift (get (m := m) : m σ) : ExceptT ε m σ) post epost ⊑
-      wp (get (m := ExceptT ε m) : ExceptT ε m σ) post epost := by
-  rfl
-
-@[lspec] theorem spec_set_ExceptT {m : Type u → Type v} {l e : Type u}
-    [CompleteLattice l] [Monad m] [LawfulMonad m] [WPMonad m l e]
-    [MonadStateOf σ m] (x : σ) (post : PUnit → l) (epost : (ε → l) × e) :
-    wp (MonadLift.monadLift (set (m := m) x : m PUnit) : ExceptT ε m PUnit) post epost ⊑
-      wp (set (m := ExceptT ε m) x : ExceptT ε m PUnit) post epost := by
-  rfl
-
-@[lspec] theorem spec_throw_ExceptT {m : Type u → Type v} {l e : Type u}
-    [CompleteLattice l] [Monad m] [LawfulMonad m] [WPMonad m l e]
-    {ε α : Type u} (err : ε) (post : α → l) (epost : (ε → l) × e) :
-    epost.1 err ⊑ wp (MonadExceptOf.throw err : ExceptT ε m α) post epost := by
-  exact WP.throw_ExceptT_wp err
+@[lspec] theorem spec_get (post : Nat → Nat → Prop) {epost : EPost⟨String → Nat → Prop⟩} :
+    (fun s => post s s) ⊑ wp (get (m := M)) post epost := PartialOrder.rel_refl
 
 @[lspec] theorem spec_pure {m : Type u → Type v} {l e : Type u}
-    [Monad m] [CompleteLattice l] [WPMonad m l e]
+    [Monad m] [CompleteLattice l] [CompleteLattice e] [WPMonad m l e]
     {α : Type u} (a : α) (post : α → l) (epost : e) :
     post a ⊑ wp (pure (f := m) a) post epost := by
   exact WPMonad.wp_pure a post epost
 
 @[lspec] theorem spec_bind {m : Type u → Type v} {l e : Type u}
-    [Monad m] [CompleteLattice l] [WPMonad m l e]
+    [Monad m] [CompleteLattice l] [CompleteLattice e] [WPMonad m l e]
     {α β : Type u} (x : m α) (f : α → m β) (post : β → l) (epost : e) :
     wp x (fun a => wp (f a) post epost) epost ⊑ wp (x >>= f) post epost :=
   WPMonad.wp_bind x f post epost
@@ -84,7 +43,7 @@ def Goal (n : Nat) : Prop := ∀ post epost, post n → wp (loop n) (fun _ => po
 set_option maxRecDepth 10000
 set_option maxHeartbeats 10000000
 
--- example : Goal 20 := by
+-- example : Goal 10 := by
 --   intro post epost h
 --   mvcgen'
 --   all_goals sorry
